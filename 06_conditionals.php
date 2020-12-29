15eeb5fe6326cfa9d4857c4410e450297ec7e472
Daniel Vega <danielalexvega@gmail.com>
2020-12-29 11:45:11 -0600

    work on conditionals and arrays

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -4,21 +4,66 @@ $age = 20;
 $salary = 300000;
 
 // Sample if
+if ($age === 20) {
+  echo "1";
+}
 
 // Without circle braces
+if ($age === 20) echo "2";
 
 // Sample if-else
+if ($age > 20) {
+  echo "too old";
+} else {
+  echo "too young";
+}
 
 // Difference between == and ===
+// just like JS
+if ($age == 20) {
+  echo "1";
+}
+if ($age == '20') {
+  echo "1";
+}
 
 // if AND
+if ($age > 20 && $salary === 300000) {
+  echo "Do Something because of the AND";
+}
+
 
 // if OR
 
+if ($age > 20 || $salary === 300000) {
+  echo "Do Something because of the AND";
+}
+
 // Ternary if
+echo $age < 22 ? 'Fresh out of school' : 'Too old';
 
 // Short ternary
 
+$age ?: 18;
+
 // Null coalescing operator
 
+$myName = isset($name) ? $name : "John";
+$myName = $name ?? "John";
+
 // switch
+$userRole = 'admin'; //editor, user, admin
+
+switch ($userRole) {
+  case 'admin':
+    echo 'admin';
+    break;
+  case 'editor':
+    echo 'editor';
+    break;
+  case 'user':
+    echo 'user';
+    break;
+  default:
+    echo 'Invalid role';
+}
--- 07_loops.php
+++ 07_loops.php
@@ -2,12 +2,53 @@
 
 // while
 
+
 // Loop with $counter
+$counter = 0;
+
+// while($counter < 10) {
+//   echo $counter . '<br>';
+//   $counter++;
+// }
 
 // do - while
+// do {
+//   echo $counter.'<br>';
+//   $counter++;
+// } while ($counter < 0);
+
 
 // for
+for ($i = 0; $i < 10; $i++) {
+  echo $i . '<br>';
+}
 
 // foreach
 
+$fruits = ['Apple', 'Banana', 'Peach', 'Orange', 'Grape'];
+// foreach ($fruits as $fruit) {
+//   echo $fruit . '<br>';
+// }
+foreach ($fruits as $i => $fruit) {
+  echo $i . ' - ' . $fruit . '<br>';
+}
+
 // Iterate Over associative array.
+$person = [
+  'name' => 'Daniel',
+  'surname' => 'Vega',
+  'age' => 34,
+  'hobbies' => ['Basketball', 'Cooking', 'Coding']
+];
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+foreach ($person as $key => $value) {
+  if (is_array($value)) {
+    echo $key . ' - ' . implode(", ", $value) . '<br>';
+  } else {
+    echo $key . ' - ' . $value . '<br>';
+  }
+}
