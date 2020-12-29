1310654d6386dd217a98638c1a98e7969af34553
Daniel Vega <danielalexvega@gmail.com>
2020-12-29 11:17:26 -0600

    work on strings and arrays

--- 04_strings.php
+++ 04_strings.php
@@ -1,12 +1,57 @@
 <?php
 
 // Create simple string
+$string = 'Hello Daniel';
+$string2 = "Hello Daniel";
+
+// echo $string . '<br>';
 
 // String concatenation
 
 // String functions
+// $string = "        Hello World          ";
+
+// echo "1 - " . strlen($string) . '<br>';
+// echo "2 - " . trim($string) . '<br>';
+// echo "3 - " . ltrim($string) . '<br>';
+// echo "4 - " . rtrim($string) . '<br>';
+// echo "5 - " . str_word_count($string) . '<br>';
+// echo "6 - " . strrev($string) . '<br>';
+// echo "7 - " . strtoupper($string) . '<br>';
+// echo "8 - " . strtolower($string) . '<br>';
+// echo "9 - " . ucfirst('hello') . '<br>';
+// echo "10 - " . lcfirst('HELLO') . '<br>';
+// echo "11 - " . ucwords('hello world') . '<br>';
+// echo "12 - " . strpos($string, 'world') . '<br>';
+// echo "13 - " . stripos($string, 'world') . '<br>';
+// echo "14 - " . substr($string, 8) . '<br>';
+// echo "15 - " . str_replace('World', 'PHP', $string) . '<br>';
+// echo "15 - " . str_ireplace("world", 'PHP', $string) . '<br>';
+
+
+
 
 // Multiline text and line breaks
+$longText = "
+  Hello, my name is Daniel
+  I am 34,
+  I love tacos.
+";
+//nl2br is a function that adds the new lines
+echo nl2br($longText);
+
+$nextText = "
+  Hello, my name is <b>Daniel</b>
+  I am <b>34</b>
+  I love sandwiches.
+";
+
+echo "1 - " . $nextText . '<br>' . '<br>';
+echo "2 - " . nl2br($nextText) . '<br>' . '<br>';
+echo "3 - " . htmlentities($nextText) . '<br>' . '<br>';
+echo "4 - " . nl2br(htmlentities($nextText)) . '<br>' . '<br>';
+
+
 
 // Multiline text and reserve html tags
 
--- 05_arrays.php
+++ 05_arrays.php
@@ -1,38 +1,104 @@
 <?php
 
 // Create array
+$fruits = ['apple', 'banana', 'grapes', 'orange'];
+$veggies = array("Cucumber", "Lettuce");
 
 // Print the whole array
 
+var_dump($fruits);
+
+echo '<br>';
+echo '<br>';
+
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
 // Get element by index
+echo $fruits[2] . '<br>';
 
 // Set element by index
 
 // Check if array has element at index 2
 
+echo isset($fruits[2]); //true
+echo isset($fruits[6]); //false
+
 // Append element
+$fruits[] = 'peach';
+
+echo '<br>';
+echo '<br>';
+
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
 
 // Print the length of the array
 
+echo count($fruits) . '<br>';
+
 // Add element at the end of the array
+
+array_push($fruits, 'strawberry');
+
+echo '<br>';
+echo '<br>';
+
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
 
 // Remove element from the end of the array
 
+echo array_pop($fruits);
+
+echo '<br>';
+echo '<br>';
+
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+
 // Add element at the beginning of the array
+
+array_unshift($fruits, 'pineapple');
 
 // Remove element from the beginning of the array
 
-// Split the string into an array
+array_shift($fruits);
 
-// Combine array elements into string
+// Split the string into an array   like split in JavaScript
+$string = "Banana, Apple, Peach";
+echo $string . "<br>";
+echo '<pre>';
+var_dump(explode(",", $string));
+echo '</pre>';
+
+// Combine array elements into string  
+echo implode("& ", $fruits) . '<br>';
 
 // Check if element exist in the array
 
+
+
 // Search element index in the array
 
+array_search('Apple', $fruits);
+
 // Merge two arrays
+$vegetables = ['Potato', 'Spinach'];
+
+echo '<pre>';
+// var_dump(array_merge($fruits, $vegetables));
+var_dump([...$fruits, ...$vegetables]);
+echo '</pre>';
+
 
 // Sorting of array (Reverse order also)
+sort($fruits);
+rsort($fruits);  // reverse
 
 
 // https://www.php.net/manual/en/ref.array.php
@@ -41,21 +107,59 @@
 // Associative arrays
 // ============================================
 
-// Create an associative array
+// Create an associative array - it's an object in JS
+$person = [
+  'name' => 'Daniel',
+  'surname' => 'Vega',
+  'age' => 34,
+  'hobbies' => ['Basketball', 'Cooking', 'Coding']
+];
+
+echo '<br>';
+echo '<br>';
+
+echo '<pre>';
+print_r($person);
+echo '</pre>';
 
 // Get element by key
+echo $person['name'] . '<br>';
 
 // Set element by key
+$person['recordLabel'] = 'Full Court Press';
 
 // Null coalescing assignment operator. Since PHP 7.4
+// if(!isset($person['address'])){
+//   $person['address'] = 'unknown';
+// };
+
+$person['address'] ??= 'unknown';
 
 // Check if array has specific key
 
+
+
 // Print the keys of the array
+echo '<pre>';
+var_dump(array_keys($person));
+echo '</pre>';
 
 // Print the values of the array
+echo '<pre>';
+var_dump(array_values($person));
+echo '</pre>';
 
 // Sorting associative arrays by values, by keys
-
+// ksort($person); // this will sort by keys
+// asort($person); // this will sort by values
 
 // Two dimensional arrays
+
+$todos = [
+  ['title' => 'Todo title 1', 'completed' => true],
+  ['title' => 'Todo title 2', 'completed' => false]
+];
+
+echo '<pre>';
+var_dump($todos);
+echo '</pre>';
